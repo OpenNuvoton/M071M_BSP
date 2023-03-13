f5e9c5ae4da24c53c6223b191d13e733b6b19c6f
YTLu <YTLu@nuvoton.com>
2023-03-13 16:07:01 +0800

    Update Readme, Revision history and chm files.
    Change-Id: I7a39ac3c1ae367288f8bc947980538820614f796

--- Library/Device/Nuvoton/M071M/Include/M071M.h
+++ Library/Device/Nuvoton/M071M/Include/M071M.h
@@ -8,7 +8,7 @@
  * @note
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ * Copyright (C) 2021~2023 Nuvoton Technology Corp. All rights reserved.
  *
  ******************************************************************************/
 
@@ -30,7 +30,7 @@
   *
   * <b>Copyright Notice</b>
   *
-  * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+  * Copyright (C) 2021~2023 Nuvoton Technology Corp. All rights reserved.
   */
 
 
